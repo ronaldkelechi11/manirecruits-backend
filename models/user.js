d929211675a9b617af9f89bfe76269b12663a5a2
Ronald Kelechi <ronaldkelechi11@gmail.com>
2022-01-01 04:34:30 +0100

    Done with user model and signup stage 1

--- models/user.js
+++ models/user.js
@@ -1,41 +1,59 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 var userSchema = new mongoose.Schema({
 
-    personal_information: {
-        firstname: {
-            type: String,
-            lowercase: true
-        },
-        lastname: {
-            type: String,
-            lowercase: true
-        },
-        username: {
-            type: String,
-            lowercase: true
-        },
-        email_address: {
-            type: String,
-            lowercase: true,
-            min: 3,
-            max: 64,
-            required: true
-        },
-        profile_picture: String,
-        date_of_birth: String
+    // PERSONAL INFORMATION
+    firstname: {
+        type: String,
+        lowercase: true
+    },
+    lastname: {
+        type: String,
+        lowercase: true
+    },
+    username: {
+        type: String,
+        lowercase: true
+    },
+    email_address: {
+        type: String,
+        lowercase: true,
+        min: 3,
+        max: 64,
+        required: true
+    },
+    profile_picture: String,
+    date_of_birth: String,
+
+    // AUTHENTICATION AND VERIFICATION
+    password: {
+        type: String,
+        required: true,
+        min: 8,
+        max: 64,
+        immutable: true
+    },
+    isVerified: {
+        type: Boolean,
+        default: false
     },
 
-    authentication: {
-        password: {
-            type: String,
-            required: true,
-            min: 8,
-            max: 64,
-            immutable: true
+    // FOLLOWING AND FOLLOWERS
+    followers: [
+        {
+            type: mongoose.SchemaTypes.ObjectId,
+            ref: "User"
         }
-    },
+    ],
+    following: [
+        {
+            type: mongoose.SchemaTypes.ObjectId,
+            ref: "User"
+        }
+    ],
 
+    // MEDIA
     posts: [
         {
             type: mongoose.SchemaTypes.ObjectId,
@@ -43,49 +61,59 @@ var userSchema = new mongoose.Schema({
         }
     ],
 
-    contact_information: {
-        phone_number: String,
-        address: {
-            street: String,
-            city: String,
-            state: String,
-            zipCode: Number,
-        }
+    // CONTACT INFORMATION
+    phone_number: String,
+    address: {
+        street: String,
+        city: String,
+        state: String,
+        zipCode: {
+            type: Number,
+            max: 6,
+            min: 5
+        },
     },
 
-    professional_information: {
-        work_experience: [],
-        education_background: [],
-        skills: [],
-        industry_job_preferences: []
+    // PROFESSIONAL INFORMATION
+    work_experience: [String],
+    education_background: [String],
+    skills: [String],
+    industry_job_preferences: [String],
+
+    // ADDITIONAL DETAILS
+    linkedin_profile: String,
+    facebook_profile: String,
+    instagram_profile: String,
+    personal_website: String,
+
+    // PREFERENCES
+    preferred_job_locations: [String],
+    desired_salary_range: {
+        type: String,
+        default: "30,000"
     },
 
-    additional_details: {
-        linkedin_profile: String,
-        facebook_profile: String,
-        instagram_profile: String,
-        personal_website: String
-    },
+    // USED FOR FEED
+    notification_preferences: [String],
 
-    preferences: {
-        preferred_job_locations: [],
-        desired_salary_range: String,
-        notification_preferences: []
-    },
-
-    interests_and_activities: {
-        hobbies: [String],
-        professional_associations: [String],
-        volunteer_work: [String]
-    }
+    // INTERESTS AND ACTIVITIES
+    hobbies: [String],
+    professional_associations: [String],
+    volunteer_work: [String]
 })
 
 
 
 // ======== PASSWORD HASHING SYSTEM ===============
 userSchema.pre('save', async function (next) {
-    const salt = await bcrypt.genSalt();
-    this.password = await bcrypt.hash(this.password, salt)
+    try {
+        const salt = await bcrypt.genSalt();
+        this.password = await bcrypt.hash(this.password, salt)
+    } catch (error) {
+        console.log('====================================');
+        console.log(error);
+        console.log('====================================');
+    }
     next();
 })
 
--- routes/signup.js
+++ routes/signup.js
@@ -2,13 +2,54 @@ const express = require('express')
 const User = require('../models/User');
 const router = express.Router()
 
-// ======== SIGNUP =========
+// ======== SIGNUP STEP 1 (email and password) =========
 router.post('/', async (req, res) => {
 
     let email = req.body.email
     let password = req.body.password
 
-    res.status(200).send(`${email} ${password}`);
+    var user = new User({
+        email_address: email,
+        password: password
+    });
+
+    await user.save().then((result) => {
+        res.status(200).send(result)
+    }).catch((err) => {
+        res.status(500).send(err)
+    });
+})
+
+
+// ======= SIGNUP STEP 2 (username,firstname,lastname,date_of_birth)===========
+router.post("/:email/2", async (req, res) => {
+
+    var email = req.params.email
+    var username = req.body.username
+    var lastname = req.body.lastname
+    var firstname = req.body.firstname
+
+    const user = await User.findOne({ email_address: email })
+        .then((result) => {
+            console.log(user);
+            res.status(200).send(result)
+        }).catch((err) => {
+            res.status(505).send(err);
+        });
+})
+
+
+//  ======= SIGNUP STEP 3 (phone_number,address)===========
+router.post("/:email/3", async (req, res) => {
+
+    var email = req.param.email
+    var phone_number = req.body.phone_number
+
+    var street = req.body.street
+    var city = req.body.city
+    var state = req.body.state
+    var zipCode = req.body.zipCode
+
 })
 
 module.exports = router
