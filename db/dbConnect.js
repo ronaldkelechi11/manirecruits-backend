525004390762416621e8af6e3df4ddd8f78e59bf
Ronald Kelechi <ronaldkelechi11@gmail.com>
2024-05-04 23:08:57 +0100

    removed env files

--- db/dbConnect.js
+++ db/dbConnect.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 
 // Pass the value of the dburl
 const connectDB = async (db_url) => {
-
     try {
         await mongoose.connect(db_url);
         return console.log('Database connected successfully ✔✔✔');
--- models/Post.js
+++ models/Post.js
@@ -3,7 +3,14 @@ const mongoose = require('mongoose');
 
 var postSchema = new mongoose.Schema({
     title: String,
-    body: String
+    body: String,
+
+
+    // Convert actual likes display to amount of likes and likes show everyone that liked
+    likes: [{
+        type: mongoose.SchemaTypes.ObjectId,
+        ref: "User"
+    }]
 })
 
 module.exports = mongoose.model("Post", postSchema)
