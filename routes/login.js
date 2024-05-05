41088a7a9c140043e741f5416c59fea5e4a92729
Ronald Kelechi <ronaldkelechi11@gmail.com>
2024-05-05 02:04:03 +0100

    Update login.js

--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,6 @@
 const express = require('express')
-const router = express.Router()
 const User = require('../models/User');
+const router = express.Router()
 const adminAuth = require('../middlewares/adminAuth');
 const userAuth = require('../middlewares/userAuth');
 
